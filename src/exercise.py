319632dc031dcba380f6b1f39961722c908e9586
MrSullivan <rsullivan@stcadocs.com>
2021-02-04 22:21:46 +0000

    variables with string, integers, float and boolean.

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,16 @@
 def main():
     #write your code below this line
+      string_as_string = input("Give a string: ")
+      integer_as_string = input("Give an integer: ")
+      float_as_string = input("Give a float: ")
+      boolean_as_string = input("Give a boolean: ")
+      print ("You gave the string " + string_as_string)
+      integer = str(integer_as_string)
+      print ("You gave the integer " + integer)
+      float_number = str(float_as_string)
+      print ("You gave the float " + float_number)
+      boolean = str(boolean_as_string)
+      print ("You gave the boolean " + boolean)
 
 if __name__ == '__main__':
     main()
